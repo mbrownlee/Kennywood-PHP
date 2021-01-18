bd0abd7037b836df59be37e5be4459332ea492f8
Michelle Brownlee <michelle@orbwave.com>
2021-01-18 13:37:56 -0600

    added park detail pages

---
+++ 2.php
@@ -0,0 +1,49 @@
+<html>
+
+<body class="container">
+
+    <style>
+        .body {
+            background-color: lightsteelblue;
+        }
+       
+    </style>
+      
+    <?php
+
+    include('nav.php');
+   
+   
+    include_once 'db_sql.php';
+    $sql = "SELECT ride.id, ride.ride_name, ride.ride_description, ride.park_area_id, park_area.area_name FROM ride INNER JOIN park_area ON ride.park_area_id = park_area.id WHERE park_area_id=2";
+    $result = mysqli_query($init, $sql);
+    echo '<img src="water.png" width="250" height="auto"  alt="water park" />' , "</br>";
+    
+    echo "<h2> Water Park Rides </h2>";
+    while ($row = mysqli_fetch_array($result)) {
+        echo "*~*~*~*~*~*~*~*~*~*~*~*~" . "</br>" . "<h3>" . $row['ride_name'] . "</h3>" . " " . $row['ride_description'] . "</br> ";
+    }
+    $sql2 = "SELECT concession.id, concession.concession_name, concession.concession_description, concession.park_area_id, park_area.area_name FROM concession INNER JOIN park_area ON concession.park_area_id = park_area.id WHERE park_area_id=2";
+    $result2 = mysqli_query($init, $sql2);
+    echo "<h2> Water Park Food </h2>";
+    while ($row = mysqli_fetch_array($result2)) {
+        echo "*~*~*~*~*~*~*~*~*~*~*~*~" . "</br>" . "<h3>" . $row['concession_name'] . "</h3>" . " " . $row['concession_description'] . "</br> ";
+    }
+
+    ?>
+    </br>
+    </br>
+ 
+    <style>
+        .container {
+            background-color: lightsteelblue;
+        }
+        h2 {
+            color: darkblue;
+            text-decoration: underline;
+        }
+
+    </style>
+</body>
+
+</html>
--- areas.php
+++ areas.php
@@ -6,31 +6,50 @@
         .body {
             background-color: lightsteelblue;
         }
-       
     </style>
-      
+
     <?php
 
     include('nav.php');
     include_once 'db_sql.php';
-   
+
     echo '<img src="carnival-party.jpg" width="250" height="auto"  alt="flags" />' . '<img src="carnival-party.jpg" width="250" height="auto"  alt="flags" />' . "</br>";
     $sql = "SELECT park_area.id, park_area.area_name, park_area.area_description FROM park_area";
     $result = mysqli_query($init, $sql);
     echo "<h2> Welcome to Kennywood!</h2>";
 
     while ($row = mysqli_fetch_array($result)) {
-        echo "*~*~*~*~*~*~*~*~*~*~*~*~" . "</br>" . "<h3>" . $row['area_name'] . "</h3>" . " " . $row['area_description'] . "</br> " ;
+
+        $url = $row['id'];
+        echo "*~*~*~*~*~*~*~*~*~*~*~*~" . "</br>" . "<h3>" . $row['area_name'] . "</h3>" . " " . $row['area_description'] . "</br> " . "<form action='$url.php'>" . "<button class='btn'>" . "Park Details" . "</button>" . "</form>" . "</br>";
+        // '<a href="'.$url.'"><input type="button" name="' .  $row["area_name"]. '" value="'. "Park Details".'"></a>'. "</br> ";
     }
 
 
     ?>
     </br>
     </br>
- 
+
     <style>
         .container {
             background-color: lightsteelblue;
+        }
+
+        .btn {
+            padding: 10px 15px;
+            background-color: darkblue;
+            color: white;
+            border: 1px solid #6D6981;
+            background-color: 250ms;
+            border-radius: 5px;
+        }
+
+        .btn:hover {
+            background-color: slateblue;
+            color: white;
+            border: 1px solid #6D6981;
+            background-color: 250ms;
+            border-radius: 5px;
         }
     </style>
 </body>
